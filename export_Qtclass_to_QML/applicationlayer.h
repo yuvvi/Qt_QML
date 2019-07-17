36998b8edcdafec4a2d79035dd12c4fd13e58c12
yuvaram <yuvaram.aligeti@gmail.com>
2019-07-17 14:50:05 +0530

    Update applicationlayer.h

--- export_Qtclass_to_QML/applicationlayer.h
+++ export_Qtclass_to_QML/applicationlayer.h
@@ -26,12 +26,11 @@ class ApplicationLayer : public QObject
     Q_OBJECT
     /*  1st arg can be given different name (other than m_printCount),
      *  But the given name will be used in QML side*/
-    //Q_PROPERTY(int m_printCount READ printCount WRITE setPrintCount NOTIFY printCountChanged)
-    Q_PROPERTY(int printCountQML READ printCount WRITE setPrintCount NOTIFY printCountChanged)
+    Q_PROPERTY(int printCountQML MEMBER m_printCount READ printCount WRITE setPrintCount NOTIFY printCountChanged)
 
     /*With change of pCount in QML side,
      * SIGNAL:printCountChanged is emitted & updates m_printCount.*/
-    Q_PROPERTY(int pCount MEMBER m_printCount NOTIFY printCountChanged)
+    #Q_PROPERTY(int pCount MEMBER m_printCount NOTIFY printCountChanged)
 public:
     explicit ApplicationLayer(QObject *parent = nullptr);
     Q_INVOKABLE void print_operation();
