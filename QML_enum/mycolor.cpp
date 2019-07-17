58e1a17d3956b02599cb85620006fbffd05e4122
yuvaram <yuvaram.aligeti@gmail.com>
2019-07-17 12:31:41 +0530

    Update mycolor.cpp
        qRegisterMetaType<MyColor::Color>("MyColorQML::Color");
        qmlRegisterUncreatableType<MyColor>("MyColorQMLModule",1,0,"MyColors","Instance not required");
    /*qmlRegisterUncreatableType - by this myColor instance cannot be created*/

--- QML_enum/mycolor.cpp
+++ QML_enum/mycolor.cpp
@@ -4,10 +4,11 @@ MyColor::MyColor(QObject *parent) : QObject(parent)
 {
 
 }
-
+/*qmlRegisterUncreatableType - by this myColor instance cannot be created*/
 void MyColor::init()
 {
     qRegisterMetaType<MyColor::Color>("MyColorQML::Color");
-    qmlRegisterType<MyColor>("MyColorQMLModule",1,0,"MyColors");
+    qmlRegisterUncreatableType<MyColor>("MyColorQMLModule",1,0,"MyColors","Instance not required");
+    //qmlRegisterType<MyColor>("MyColorQMLModule",1,0,"MyColors");
 }
 
